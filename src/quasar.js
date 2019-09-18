63648eee546469af45765c29ebf5f10335e1a343
Theodor Costea <costea@bbaw.de>
2019-09-18 12:41:44 +0200

    Add Quasar plugin

---
+++ src/quasar.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+import './styles/quasar.styl';
+import lang from 'quasar/lang/de.js';
+import '@quasar/extras/material-icons/material-icons.css';
+import Quasar from 'quasar';
+
+Vue.use(Quasar, {
+  config: {},
+  lang,
+});
