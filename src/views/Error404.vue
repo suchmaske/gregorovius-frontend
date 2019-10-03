4e6b55970dc6b18334f378f7a0c5c2e0c6cc06bb
Theo Costea <theo.costea@gmail.com>
2019-10-03 10:17:20 +0200

    Translate 404 page

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -6,12 +6,12 @@
         style="width:30vw;max-width:150px;"
       >
     </p>
-    <p class="text-faded">Sorry, nothing here...<strong>(404)</strong></p>
+    <p class="text-faded">Hier ist leider nichts zu finden...<strong>(404)</strong></p>
     <q-btn
       color="secondary"
       style="width:200px;"
       @click="$router.go(-1)"
-    >Go back</q-btn>
+    >Zurück</q-btn>
   </div>
 </template>
 
