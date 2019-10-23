b78459d2ca09a94dd3950e101f691c7936087f90
Theodor Costea <costea@bbaw.de>
2019-10-23 14:34:43 +0200

    Change API URL to HTTPS

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
-export const API = 'http://gregorovius-edition.dhi-roma.it/api';
+export const API = 'https://gregorovius-edition.dhi-roma.it/api';
 export const XSLTToken = '3h3gbl4a2r92cb7cx05bhsg7';
