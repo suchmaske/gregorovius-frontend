e757a2aecf6e9812b7f810636c278e56f7e6325c
Theo Costea <theo.costea@gmail.com>
2020-07-23 22:53:42 +0200

    Revert "LettersIndex: added context menu on right-click"
    This reverts commit 02613a6d4b5e4758b2e71437ec5e5591700d8dae.

--- src/views/LettersIndex.vue
+++ src/views/LettersIndex.vue
@@ -56,22 +56,6 @@
               :class="searchInput ? 'cursor-pointer g-searchrow' : 'cursor-pointer'"
               @click.native="$router.push({ name: 'Brief', params: { id: props.row.id } })"
             >
-
-              <q-menu
-                touch-position
-                context-menu
-              >
-                <q-list dense style="min-width: 100px">
-                  <q-item @click.native="openItem(props.row.id, 'window')" clickable v-close-popup>
-                    <q-item-section>In neuem Fenster öffnen</q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item @click.native="openItem(props.row.id, 'tab')" clickable v-close-popup>
-                    <q-item-section>In neuem Tab öffnen</q-item-section>
-                  </q-item>
-                </q-list>
-              </q-menu>
-
               <q-td key="desc" :props="props">
                 {{ props.row.name }}
                 <q-btn
@@ -417,20 +401,6 @@ export default {
       );
     },
 
-    openItem(id, target) {
-      let routeData = this.$router.resolve({ name: 'Brief', params: { id: id } })
-      switch (target) {
-        case 'tab':
-          window.open(routeData.href, '_blank');
-          break;
-        case 'window':
-          const width = window.outerWidth
-          const height = window.outerHeight
-          const strWindowFeatures = `resizable,location=yes,height=${height},width=${width},scrollbars=yes,status=yes`
-          window.open(routeData.href, '_blank', strWindowFeatures);
-          break;
-      }
-    },
     loadQueryToStore() {}
   }
 };
